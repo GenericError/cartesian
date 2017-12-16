a1102e4641781e9f38def1410cc92c42b25309dd
Luke Stewart <stewartluke21@yahoo.com>
2017-12-16 13:46:25 +1100

    Added quadrant() function to Point class
    Added a function that determines what quadrant of the cartesian plane the point is located in, as well as the tests that confirm it is correct

--- cartesian.py
+++ cartesian.py
@@ -13,3 +13,18 @@ class Point(object):
 
     def __str__(self):
         return "Point at x=" + str(self.x) + " and y=" + str(self.y)
+
+    def quadrant(self):
+        """ Determines the quadrant that the point is located in """
+        if self.x == 0 or self.y == 0:
+            return None
+        elif self.x > 0 and self.y > 0:
+            return 1
+        elif self.x < 0 and self.y > 0:
+            return 2
+        elif self.x < 0 and self.y < 0:
+            return 3
+        elif self.x > 0 and self.y < 0:
+            return 4
+        else:
+            return None
--- tests.py
+++ tests.py
@@ -13,8 +13,7 @@ class TestPointClass(unittest.TestCase):
         self.assertNotEqual(point, False)
 
     def test_create_point_with_coords(self):
-        """ Tests that the coordinates are correctly
-        assigned upon creating a point """
+        """ Ensures providied coordinates are correctly assigned """
         point = cartesian.Point(-1, 2)
         self.assertEqual(point.x, -1)
         self.assertEqual(point.y, 2)
@@ -36,6 +35,35 @@ class TestPointClass(unittest.TestCase):
         point = cartesian.Point(-1, 2)
         self.assertEqual(point.__str__(), "Point at x=-1 and y=2")
         self.assertEqual(str(point), "Point at x=-1 and y=2")
+
+    def test_point_quadrant_none(self):
+        """ Ensures that a point lying on an axis has no quadrant """
+        points = [
+            cartesian.Point(1, 0),
+            cartesian.Point(0, 1),
+            cartesian.Point(-1, 0),
+            cartesian.Point(0, -1),
+            cartesian.Point(0, 0)
+        ]
+
+        for point in points:
+            self.assertEqual(point.quadrant(), None)
+
+    def test_point_quadrant_first(self):
+        """ Ensures that the first quadrant is correctly identified """
+        self.assertEqual(cartesian.Point(1,1).quadrant(), 1)
+
+    def test_point_quadrant_second(self):
+        """ Ensures that the second quadrant is correctly identified """
+        self.assertEqual(cartesian.Point(-1,1).quadrant(), 2)
+
+    def test_point_quadrant_third(self):
+        """ Ensures that the third quadrant is correctly identified """
+        self.assertEqual(cartesian.Point(-1,-1).quadrant(), 3)
+
+    def test_point_quadrant_fourth(self):
+        """ Ensures that the fourth quadrant is correctly identified """
+        self.assertEqual(cartesian.Point(1,-1).quadrant(), 4)
 
 
 if __name__ == '__main__':
