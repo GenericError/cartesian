4a7579a2d70142da425b64289fc327ad9755e7f7
Luke Stewart <stewartluke21@yahoo.com>
2017-12-16 12:29:55 +1100

    Created tests.py
    Created the test file and implemented one test that creates a Point instance

--- cartesian.py
+++ cartesian.py
@@ -1,2 +1,5 @@
+""" Cartesian - a simple representation of a cartesian plane """
+
 class Point(object):
+    """ A basic point with x and y coordinates"""
     pass
---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+import cartesian
+
+class TestPointClass(unittest.TestCase):
+
+    def test_create_point(self):
+        point = cartesian.Point()
+
+if __name__ == '__main__':
+    unittest.main()
